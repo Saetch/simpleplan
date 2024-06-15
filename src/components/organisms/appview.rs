4726db279703d9ae9af4f8c8c9fcb280023b6cdf
Saetch <nils.wriedt@hotmail.com>
2024-06-15 19:10:14 +0200

    fixed broken key entries in data

--- src/components/organisms/appview.rs
+++ src/components/organisms/appview.rs
@@ -94,6 +94,41 @@ fn fix_data(mut data: Daten) -> Daten{
     let (key, value) = key_value.iter().next().unwrap();
     let key = key.clone();
     data.lebensmittel.remove("Möhregegartgegart");
-    data.lebensmittel.get_mut("Möhregegart").unwrap().insert(key.clone(), value.clone());
+    data.lebensmittel.get_mut("Möhregegart").unwrap().insert(key, *value);
+
+
+
+    let special_words = ["gebraten", "gekocht", "gegart", "gegarts"];
+    let mut vec_to_update = Vec::new();
+    for (key, _) in data.lebensmittel.iter(){
+        for word in special_words.iter(){
+            if key.ends_with(word){
+                vec_to_update.push(key.clone());
+            }
+        }
+    }
+    
+    for to_update_key in vec_to_update.iter(){
+        let mut splits = to_update_key.split("ge").map(|s| s.to_string()).collect::<Vec<String>>();
+        for split in splits[1..].iter_mut(){
+            split.insert_str(0, "ge");
+        }
+        splits.insert(splits.len()-1, " ".to_owned());
+        let new_key = splits.join("");
+        let hash_map = data.lebensmittel.get(to_update_key).unwrap().clone();
+        data.lebensmittel.remove(to_update_key);
+        data.lebensmittel.insert(new_key, hash_map);
+
+    }
+
+    let fixable_key = "Vollkornrei gegarts";
+    let key_value = data.lebensmittel.get(fixable_key).unwrap().clone();
+    data.lebensmittel.remove(fixable_key);
+    let mut lm = data.lebensmittel.get_mut("Vollkornreis gegart");
+    for (key, value) in key_value.iter(){
+        lm.as_mut().unwrap().insert(key.clone(), *value);
+    }
+
+
     return data;
 }
